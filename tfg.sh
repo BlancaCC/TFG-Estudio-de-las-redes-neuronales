5483ac901cf7c63efc5dd74f0dec4287a7e2f2f6
BlancaCC <blancacano2.71828@gmail.com>
2021-10-09 15:55:07 +0200

    Bernstein: Añado script para agilizar el acceso al tfg (este necesita de un .desktop que lo llame

---
+++ tfg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Abrir carpeta de material
+# nautilus -w /home/blanca/repositorio/TFG &
+
+# Abrir el código fuente en vscode
+code /home/blanca/repositorio/TFG/doc &
+
+# Abrir tfg.pdf
+evince /home/blanca/repositorio/TFG/doc/tfg.pdf &
