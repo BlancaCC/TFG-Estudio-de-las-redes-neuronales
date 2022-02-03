7512bd43d684ecf22742a0a68b08703dddd274a8
Blanca <blancacano2.71828@gmail.com>
2022-02-03 14:05:18 +0100

    Spell cheking funcionando #54 Puede que de error el exit, ya que solo considera el del último (no sé cómo se lo va a tragar el de github)

--- spell-check.sh
+++ spell-check.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-
-MyPrint () {
+SpellingError () {
     error=0;
     echo "";
-    echo "Errores ortográficos en fichero" $1":";
+    echo "Analizando si hay fallos en fichero" $1":";
     n_line=1; 
     while IFS= read -r line
     do
         let "n_line += 1"
-        resultado=$(echo $line | aspell --mode=tex  --lang=en --list | aspell --mode=tex  --lang=es --list);
+        resultado=$(echo $line | aspell --mode=tex  --lang=en --list | aspell --mode=tex  --lang=es --list --home-dir=. --personal=.github/workflows/aspell-exceptions.txt);
         [ -z "$resultado" ] ||( echo "Spell error in line $n_line : $resultado" && let "error = 1")
     done < $1
     exit $error; 
 }
-
-export -f MyPrint; 
-
-#MyPrint "doc/tfg.tex"; 
-find doc/capitulos -name "*.tex" -exec bash -c 'MyPrint "{}"' \;
+export -f SpellingError; 
+find doc/capitulos -name "*.tex" -exec bash -c 'SpellingError "{}"' \;
 
 
