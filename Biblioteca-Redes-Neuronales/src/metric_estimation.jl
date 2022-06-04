4a9b7c9ff1c3ca9aa6b5cf5e0ce0478a067217a4
Blanca <blancacano2.71828@gmail.com>
2022-06-04 18:36:43 +0200

    Arregla test de metric_estimation #116

--- Biblioteca-Redes-Neuronales/src/metric_estimation.jl
+++ Biblioteca-Redes-Neuronales/src/metric_estimation.jl
@@ -7,11 +7,14 @@ using Statistics
 using LinearAlgebra
 export Regression
 
-function Regression(X,Y,f)
+function Regression(X::Vector,Y,f)
+    f_x = map(f, X)
+    diferences = map(norm,eachrow(Y .- f_x))
+    return mean(diferences), median(diferences), std(diferences), cor(Y, f_x)
+end 
+function Regression(X::Matrix,Y,f)
     f_x = map(x->f(x)[1], eachrow(X))
     diferences = map(norm,eachrow(Y .- f_x))
-    println(Y)
-    println(f_x)
     return mean(diferences), median(diferences), std(diferences), cor(Y, f_x)
 end 
 
--- Biblioteca-Redes-Neuronales/test/metric_estimation.test.jl
+++ Biblioteca-Redes-Neuronales/test/metric_estimation.test.jl
@@ -6,7 +6,7 @@ include("../src/metric_estimation.jl")
 
 @testset "Regression metrics" begin
     f(x)=x.*x
-    X = reshape([1,-1,-2,2],(4,1))
-    Y = map(f, eachrow(X))
-    @test Metric.Regression(X,Y,f) == (0,0,0)
+    X = [1,-1,-2,2]
+    Y = map(f, X)
+    @test Metric.Regression(X,Y,f) == (0,0,0,1)
 end
