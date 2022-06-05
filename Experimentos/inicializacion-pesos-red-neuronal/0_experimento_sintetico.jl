011cb5127d088ed1e2d8c50362f89bf7140b1a5b
Blanca <blancacano2.71828@gmail.com>
2022-06-05 09:08:26 +0200

    Añade gráficos inicialización de pesos correctos R->R #116 El fallo estaba en el dominio entonces, porque al hacer el rango equiespacioado ahora salen bien los gráficos

--- Experimentos/inicializacion-pesos-red-neuronal/0_experimento_sintetico.jl
+++ Experimentos/inicializacion-pesos-red-neuronal/0_experimento_sintetico.jl
@@ -3,6 +3,10 @@
 ########################################################
 using Random
 using Plots
+using TOML
+FICHERO_CONFIGURACION = "Experimentos/.config.toml"
+config = TOML.parsefile(FICHERO_CONFIGURACION)["visualizacion-inicializacion-pesos-R"]
+img_path = config["DIRECTORIO_IMAGENES"]
 
 Random.seed!(1)
 include("../../Biblioteca-Redes-Neuronales/src/initial_neuronal_network.jl")
@@ -23,16 +27,14 @@ for (data_set_size,n) in zip([3,4,5, 8,15,23,51,73,100, 103],[2,3,5,7,10,20,51,7
     println("EXPERIMENTO SINTÉTICO")
     println("n=$n y tamaño conjunto $data_set_size")
     # Número de neuronas 
-    X_train= map(
-            x-> (x-0.5)*K_range*2, # reescalamos al intervalo [-5,5]
-            rand(Float64, data_set_size)
-    )
+    X_train= Vector(LinRange(-K_range, K_range, n))
     Y_train = map(f_regression, X_train)
     h = InitializeNodes(X_train, Y_train, n, M)
     evaluate(x)=OneLayerNeuralNetwork.ForwardPropagation(h,
         ActivationFunctions.RampFunction,x)
-    #@show Metric.Regression(X_train, Y_train, evaluate)
     interval = [-K_range,K_range] 
+    file_name = "f_ideal_y_rn_con_$(n)_neuronas"
     plot(x->evaluate([x])[1], -K_range,K_range, label="red neuronal n=$n")
-    display(plot!(f_regression, label="f ideal", title="Comparativa función ideal y red neuronal n=$n"))
+    plot!(f_regression, label="f ideal", title="Comparativa función ideal y red neuronal n=$n")
+    png(img_path*file_name)
 end
