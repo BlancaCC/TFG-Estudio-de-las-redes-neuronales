3ccb98cc0a7c0cc56c3e3a5a34dc0bf2be533cc4
Blanca <blancacano2.71828@gmail.com>
2022-06-11 13:08:55 +0200

    Elimina variable x_aux de forward propagation  #117

--- OptimimizedNeuralNetwork.jl/src/forward_propagation.jl
+++ OptimimizedNeuralNetwork.jl/src/forward_propagation.jl
@@ -7,10 +7,8 @@ export forward_propagation
 forward_propagation (h::AbstractOneLayerNeuralNetwork, activation_function, x::Vector{Real})
 Only use an activation function
 """
-function forward_propagation(h,activation_function, x)
-    x_aux = copy(x)
-    s = h.W1 * push!(x_aux,1)
+function forward_propagation(h::AbstractOneLayerNeuralNetwork,activation_function, x)
+    s = h.W1 * push!(copy(x),1)
     ∑= map(activation_function,s)
-    x_aux = h.W2 * ∑
-    return x_aux
+    return h.W2 * ∑
 end 
